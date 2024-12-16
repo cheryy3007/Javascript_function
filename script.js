9137f2e522fa8910431546950171b7b4e3c70cb2
cheryy3007 <zulfiyagazna@gmail.com>
2024-12-16 17:42:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function switchToLightMode() {
+  document.body.classList.remove('dark-mode');
+  document.body.classList.add('light-mode');
+}
+
+function switchToDarkMode() {
+  document.body.classList.remove('light-mode');
+  document.body.classList.add('dark-mode');
+}
